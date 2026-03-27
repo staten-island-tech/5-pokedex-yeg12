c3b5641c381fd558996be2e6bdc34f1e44059d89
yeg12 <yeg@nycstudents.net>
2026-03-27 10:17:24 -0400

    daw

--- et.py
+++ et.py
@@ -1,7 +1,16 @@
+# import json
+# pokedex = open ("./pokedex.json", encoding="utf8")
+# data = json.load(pokedex)
+# def pokemon():
+#     for s in data:
+#         print(s["name"]["english"])
+# pokemon()
+
+
 import json
 pokedex = open ("./pokedex.json", encoding="utf8")
 data = json.load(pokedex)
 def pokemon():
-    for s in data:
-        print(s["name"]["english"])
+    user = input("what pkemone u wnat")
+    lang = input("what language u wnat")
 pokemon()
