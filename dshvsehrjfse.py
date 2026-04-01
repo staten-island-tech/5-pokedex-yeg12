1146dc3e63c6507ef06df36ba97ab920d25f25ac
yeg12 <yeg@nycstudents.net>
2026-04-01 10:17:12 -0400

    sdf

--- dshvsehrjfse.py
+++ dshvsehrjfse.py
@@ -1,25 +1,47 @@
-sushi_orders = [
-    {"name": "California Roll", "price": 8},
-    {"name": "Spicy Tuna Roll", "price": 10},
-    {"name": "Salmon Nigiri", "price": 6},
-    {"name": "California Roll", "price": 8},
-    {"name": "Dragon Roll", "price": 12},
-    {"name": "Spicy Tuna Roll", "price": 10},
-    {"name": "Miso Soup", "price": 4},
-    {"name": "Edamame", "price": 5},
-    {"name": "Salmon Nigiri", "price": 6},
-    {"name": "California Roll", "price": 8}
-]
+# sushi_orders = [
+#     {"name": "California Roll", "price": 8},
+#     {"name": "Spicy Tuna Roll", "price": 10},
+#     {"name": "Salmon Nigiri", "price": 6},
+#     {"name": "California Roll", "price": 8},
+#     {"name": "Dragon Roll", "price": 12},
+#     {"name": "Spicy Tuna Roll", "price": 10},
+#     {"name": "Miso Soup", "price": 4},
+#     {"name": "Edamame", "price": 5},
+#     {"name": "Salmon Nigiri", "price": 6},
+#     {"name": "California Roll", "price": 8}
+# ]
 
-def receipt(orders):
-    the_recipt = {}
-    for i in orders:
-        if i ["name"] in the_recipt:
-            continue
+# def receipt(orders):
+#     the_recipt = {}
+#     for i in orders:
+#         if i ["name"] in the_recipt:
+#             continue
+#         else:
+#             the_recipt[i["name"]] = {
+#                 "price": i["price"],
+#                 "qty": 1
+#             }
+#         for sushi, value in the_recipt.items():
+#             price = value ["price"]*value["qty"]
+#             print(sushi, value["qty"],price)
+# receipt(sushi_orders)
+
+
+
+wards = {
+    "Cardiology":  ["Alice", "Bob", "Carol"],
+    "Neurology":   ["Diana", "Eve"],
+    "Orthopedics": ["Frank", "Grace", "Hank"],
+    "Oncology":    ["Ivy", "Bob"]
+}
+
+staff = {}
+for dept, docs in wards.items():
+    for doc in docs:
+        if doc not in staff:
+            staff[doc] = [dept]
         else:
-            the_recipt[i["name"]] = {
-                "price": i["price"],
-                "qty": 1
-            }
-    print(the_recipt)
-receipt(sushi_orders)
+            staff[doc].append(dept)
+                
+print(staff["Bob"])
+            
