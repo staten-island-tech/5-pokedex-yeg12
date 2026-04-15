2b1886b00f4d0a52105f1d8b06f5fb33131568d0
yeg12 <yeg@nycstudents.net>
2026-04-15 10:16:45 -0400

    d

--- et.py
+++ et.py
@@ -7,12 +7,26 @@
 # pokemon()
 
 
+# import json
+# pokedex = open ("./pokedex.json", encoding="utf8")
+# data = json.load(pokedex)
+
+# def pokemon():
+#     user = input("what pokemon u want   ")
+#     lang = input("Waht language u want  ")
+#     for v in data :
+#         if v["name"]["english"] == user:
+#             print(v["name"][lang])
+# pokemon()
+        
 
 import json
 pokedex = open ("./pokedex.json", encoding="utf8")
 data = json.load(pokedex)
-d = input("what pokemnon")
-for i in data:
-    if d == i:
-        
 
+def pokemon():
+    type = input("hwat type of pokemon  ")
+    for a in data:
+        if a["type"] == type:
+            print(a["name"])
+pokemon()
