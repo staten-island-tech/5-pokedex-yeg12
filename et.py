41a7fe44d0d6e1a6ebbcedb238cb0c19878fe1ee
yeg12 <yeg@nycstudents.net>
2026-04-23 09:46:56 -0400

    Update et.py

--- et.py
+++ et.py
@@ -1,23 +1,23 @@
-import json
+""" import json
 pokedex = open ("./pokedex.json", encoding="utf8")
 data = json.load(pokedex)
 def pokemon():
     for s in data:
         print(s["name"]["english"])
+pokemon() """
+
+
+import json
+pokedex = open ("./pokedex.json", encoding="utf8")
+data = json.load(pokedex)
+
+def pokemon():
+    user = input("what pokemon u want   ")
+    lang = input("Waht language u want  ")
+    for v in data :
+        if v["name"]["english"] == user:
+            print(v["name"][lang])
 pokemon()
-
-
-# import json
-# pokedex = open ("./pokedex.json", encoding="utf8")
-# data = json.load(pokedex)
-
-# def pokemon():
-#     user = input("what pokemon u want   ")
-#     lang = input("Waht language u want  ")
-#     for v in data :
-#         if v["name"]["english"] == user:
-#             print(v["name"][lang])
-# pokemon()
         
 
 # import json
